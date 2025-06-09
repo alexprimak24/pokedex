6796ea60d8d533f5d69d85507b911e31d66fbe35
alexprimak24 <aprimak43@gmail.com>
2025-06-09 18:15:46 +0000

    built cleanInput func and tested it

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	fmt.Printf("Hello, World!")	
+}
+
+func cleanInput(text string) []string {
+	return strings.Fields(strings.ToLower(text))
 }
 
